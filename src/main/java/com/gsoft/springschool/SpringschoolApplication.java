218d15081b892ae66c3da77594ca7bd30fc50e81
Rafa Gayoso <gayoso0597@gmail.com>
2022-02-22 16:17:33 +0100

    Added project Files

---
+++ src/main/java/com/gsoft/springschool/SpringschoolApplication.java
@@ -0,0 +1,13 @@
+package com.gsoft.springschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringschoolApplication.class, args);
+	}
+
+}
